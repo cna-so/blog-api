80b9f8b5f9c719b4df6ecc5c27368a0258531c0e
cna <cna.so@outlook.com>
2023-05-11 13:11:55 +0330

    002 - design database v0.01 beta

---
+++ models/databse/init_create_tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users
+(
+    id         SERIAL PRIMARY KEY,
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(50),
+    last_name  VARCHAR(80),
+    password   VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+CREATE TABLE articles
+(
+    id          SERIAL PRIMARY KEY,
+    title       VARCHAR(200) NOT NULL,
+    description TEXT         NOT NULL,
+    creator     INTEGER REFERENCES users (id),
+    created_at  TIMESTAMP,
+    updated_at  TIMESTAMP
+);
