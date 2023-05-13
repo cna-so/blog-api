1c93da1973e8031c31747d4e793e2bb58cf49ae8
cna <cna.so@outlook.com>
2023-05-13 11:09:54 +0330

    002 - connect to database

---
+++ cmd/api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "backend/initializer"
+
+func init() {
+	initializer.ConnectToDb()
+}
+
+func main() {
+
+}
---
+++ initializer/connectToDb.go
@@ -0,0 +1,24 @@
+package initializer
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/jackc/pgx/v5"
+	"log"
+)
+
+var Db *sql.DB
+
+func ConnectToDb() {
+	var err error
+	Db, err = sql.Open("pgx", "host=localhost user=cna password=1388 dbname=weblog sslmode=disable")
+	if err != nil {
+		log.Fatal("cannot connect to database", err)
+	}
+	fmt.Println("connected to database")
+	err = Db.Ping()
+	if err != nil {
+		log.Fatal("cannot ping to database", err)
+	}
+	fmt.Println("ping database successfully")
+}
