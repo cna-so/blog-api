3fca1a98a5cd59977c28067e75f7639d81f2ab2b
cna <cna.so@outlook.com>
2023-05-13 12:17:41 +0330

    008 - add server check

--- controllers/routes.go
+++ controllers/routes.go
@@ -3,11 +3,18 @@ package controllers
 import (
 	controllers "backend/controllers/user"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Routes() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
+	router.GET("/ping", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+
 	user := router.Group("/api/v1/user")
 	user.POST("/create", controllers.CreateUser)
 
